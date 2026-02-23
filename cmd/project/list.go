27250d6868873969df3176bacdb95af10861d089
キムテソン <maruuchi@pencil.co.jp>
2026-02-23 23:58:31 +0900

    [#4]feat：プロジェクトコマンド（list/set/current）を実装

--- cmd/project/list.go
+++ cmd/project/list.go
@@ -1,13 +1,39 @@
 package project
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/KimMaru10/bl-cli/internal/cmdutil"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/spf13/cobra"
+)
+
+var headerStyle = lipgloss.NewStyle().Bold(true)
 
 func newListCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "list",
 		Short: "プロジェクト一覧を表示する",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO: implement
+			_, client, err := cmdutil.LoadConfigAndClient()
+			if err != nil {
+				return err
+			}
+
+			projects, err := client.GetProjects()
+			if err != nil {
+				return err
+			}
+
+			if len(projects) == 0 {
+				fmt.Println("プロジェクトがありません")
+				return nil
+			}
+
+			fmt.Printf("%s\t%s\n", headerStyle.Render("KEY"), headerStyle.Render("NAME"))
+			for _, p := range projects {
+				fmt.Printf("%s\t%s\n", p.ProjectKey, p.Name)
+			}
 			return nil
 		},
 	}
